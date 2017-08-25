079a869cc43e3b63c2ab9b571e983764deab494b
dbg <beganovic.dorian@gmail.com>
2017-08-25 19:22:31 +0200

    features: fixed default spring boot logging pattern

--- src/main/java/cz/zcu/kiv/Server/SparkController.java
+++ src/main/java/cz/zcu/kiv/Server/SparkController.java
@@ -119,13 +119,7 @@ public class SparkController {
 
     @ExceptionHandler({Exception.class})
     public String error(Exception e) {
-        // Nothing to do.  Returns the logical view name of an error page, passed
-        // to the view-resolver(s) in usual way.
-        // Note that the exception is NOT available to this view (it is not added
-        // to the model) but see "Extending ExceptionHandlerExceptionResolver"
-        // below.
         logger.error(e.getMessage());
-
         return e.getMessage();
     }
 
